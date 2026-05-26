3bf94037587b3fc13042a0b3628f08799839d2e2
codep1ltio <Joonasmikael5@gmail.com>
2026-05-26 17:27:48 +0700

    logic & slight refactoring

--- src/logic.rs
+++ src/logic.rs
@@ -1,19 +1,7 @@
 mod server;
 use server::*;
 
-#[macro_export]
-macro_rules! time {
-    ($name:expr, $block:block) => {{
-        let time = std::time::Instant::now();
-        let result = $block;
-        let dur = time.elapsed();
-        println!(
-            ">Task: \x1b[32m{}\x1b[0m\n>Time: \x1b[34m{:.1?}\x1b[0m",
-            $name, dur
-        );
-        result
-    }};
-}
+pub mod lib;
 
 pub struct Launcher {
     ui: MainWindow,
